5ee51d4280436480e9b969dab9b4efa4a379b62c
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-10-27 18:56:21 +0200

    push 3

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,104 @@
 // es1:
+
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-let parola = prompt("inserisci la parola da controllare");
-let inverso = "";
-let condizione_1 = console.log (`${parola} è una parola palindroma`)
-let condizione_2 = console.log (`${parola} non è una parola palindroma`)
+// let parola = prompt("inserisci la parola da controllare");
+// let inverso = "";
+// let condizione_1 = console.log (`${parola} è una parola palindroma`)
+// let condizione_2 = console.log (`${parola} non è una parola palindroma`)
 
 
-    function palindroma(){
+//     function palindroma(){
 
-        for (let i = parola.length -1 ; i >= 0; i--) {
+//         for (let i = parola.length -1 ; i >= 0; i--) {
     
-        let palindroma = inverso += parola[i];
-    }
-    return inverso;
+//         let palindroma = inverso += parola[i];
+//     }
+//     return inverso;
  
-    }
+//     }
    
 
     
-    if(parola === palindroma()){
+//     if(parola === palindroma()){
 
-        console.log (`${parola} è una parola palindroma`)
+//         console.log (`${parola} è una parola palindroma`)
+
+//     } else if (parola !== palindroma()){
+//         console.log (`${parola} non è una parola palindroma`)
         
-    } else if (parola !== palindroma()){
-        console.log (`${parola} non è una parola palindroma`)
-        
+//     }
+
+// es:2 
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+   
+   
+    
+    let scegli = prompt(`scegli pari o dispari`)
+
+
+    if (scegli != "pari" && scegli !="dispari") {
+    alert("devi sceglioere se pari o dispari");
     }
+
+    let scegli_numero = parseInt(prompt("inserisci un numero da 1 a 5"));
+
+    let genera_numero = Math.floor(Math.random() * 5)
+
+    let pc = alert(`pc ha inserito ${genera_numero}` )
+
+    let somma =scegli_numero + genera_numero
+
+    if (somma % 2 == 0 && scegli === "pari") {
+        alert("hai vinto")
+    }
+
+    else if (somma % 2 == 0 && scegli !== "pari") {
+        alert("hai perso")
+    }
+
+    else if (somma % 2 != 0 && scegli === "dispari"){
+        alert("hai vinto")
+    }
+
+    else if (somma % 2 != 0 && scegli !== "dispari"){
+        alert("hai perso")
+    }
+
+
+    
+
+     
+
+        
+    
+    
+    
+   
+
+    
+
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
          
          
     
